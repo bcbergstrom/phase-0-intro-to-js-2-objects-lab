a6560d066af0409e20ab072eb4f93cc983458c81
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-04-15 20:29:01 -0600

    solved

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+	// Write your solution in this file!
+   const employee = {name: null, streetAddress: null}
+   
+    function updateEmployeeWithKeyAndValue(employee, key, value){
+        const temp = {...employee}
+        temp[key] = value;
+        return temp;
+    }
+    
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key] = value;
+        return employee;
+    }
+    function deleteFromEmployeeByKey(employee, key){
+        const temp = {...employee};
+        delete temp[key];
+        return temp;
+    }
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+        delete employee[key];
+        return employee;
+    }
